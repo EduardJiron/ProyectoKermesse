5aeff28b8326b6400669d887a871c51f5974b65b
EduardJiron <eduard.jiron13901@est.uca.edu.ni>
2022-11-13 15:21:09 -0300

    fix reference

--- index.php
+++ index.php
@@ -8,8 +8,11 @@
         <meta name="author" content="" />
         <title>Dashboard - SB Admin</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
-        <link href="css/styles3.css" rel="stylesheet" />
-        <link rel="stylesheet" href="css/styles4.css">
+        
+        <link href="css/styles6.css" rel="stylesheet" />
+        <link href="css/styles7.css" rel="stylesheet" />
+
+        
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
