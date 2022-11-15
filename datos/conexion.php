9bfe5d364177d44a6c83293e620dfee6e1e5f96b
BenjaminNarvaez-est <benjamin.narvaez13341@est.uca.edu.ni>
2022-11-15 03:13:41 -0600

    Agregar y editar de Ingreso comunidad e ingreso comunidad Det

--- datos/conexion.php
+++ datos/conexion.php
@@ -14,7 +14,7 @@ class Conexion
     public function conectar()
 	{
         $serverName = 'localhost';
-        $dbName = 'webk';
+        $dbName = 'dbkermesse';
         $userName = 'root';
 
         $pwd = 'Temporal2022+';
