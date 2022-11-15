51bef8ee80e38476eb1a0c1f89c460367d58a82c
EduardJiron <eduard.jiron13901@est.uca.edu.ni>
2022-11-15 05:14:15 -0300

    menu

--- editardenominacion.php
+++ editardenominacion.php
@@ -63,67 +63,94 @@ $par= new Denominacion();
                 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                     <div class="sb-sidenav-menu">
                         <div class="nav">
-                            <div class="sb-sidenav-menu-heading">Core</div>
-                            <a class="nav-link" href="index.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Dashboard
-                            </a>
-                            <div class="sb-sidenav-menu-heading">Usuario</div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                Gestionar usuario
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="layout-static.html">Crear Usuario</a>
-                                    
-                                    <a class="nav-link" href="layout-sidenav-light.html">Editar Usuario</a>
-                                    <a class="nav-link" href="layout-sidenav-light.html">Borrar Usuario</a>
-                                </nav>
-                            </div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                                Listado
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                                  
+                        <div class="sb-sidenav-menu-heading">gestionar usuario</div>
+                        <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
                                     <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                                        Listar Usuario
+                                        Usuarios y roles
                                         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                                     </a>
                                     
     
                                    <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                                        <!--<nav class="sb-sidenav-menu-nested nav">
+                                        <nav class="sb-sidenav-menu-nested nav">
+                                            <a class="nav-link" href="tblusuario.php">Usuarios</a>
+                                            <a class="nav-link" href="tblrol.php">rol</a>
+                                            <a class="nav-link" href="tblopciones.php">opciones</a>
+                                            <a class="nav-link" href="tblopciones.php">asignar rol</a>
+                                        </nav>
+                                    </div>
+                      
+                           
+                            <div class="sb-sidenav-menu-heading">kermesse
+
+                            </div>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Producto y precios
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="tblproducto.php">Productos</a>
+                                    
+                                    <a class="nav-link" href="tblctgproducto.php">Categorias</a>
+                                    <a class="nav-link" href="tblListaPrecioDet.php">precios</a>
+                                    <a class="nav-link" href="tblListaPrecio.php">lista de precio</a>
+                                    <a class="nav-link" href="tblconBono.php">bonos</a>
+                                   
+                                </nav>
+                            </div>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                                Ingresos y gastos
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                               
+                                    </a>
+                                             
+                                       <a class="nav-link" href="tblIngresoComunidad.php">Ingreso comunidad</a>
+                                    <a class="nav-link" href="tblIngresoComunidadDet.php">Ingresos comunidad det</a>
+                                    <a class="nav-link" href="tblListaPrecio.php">lista de precio</a>
+                                    <a class="nav-link" href="tblmoneda.php">moneda</a>
+                                    <a class="nav-link" href="tblgastos.php">gasto</a>
+
+                                    <a class="nav-link" href="tblgastosdet.php">gasto det</a>
+                                      <a class="nav-link" href="tblconBono.php">bono</a>
+                                    <a class="nav-link" href="tblDenominacion.php">Denominacion</a>
+                                    <a class="nav-link" href="tblCategoriaGastos.php">categoria de gasto</a>
+                                  
+                                    <a class="nav-link" href="tbltasaCambio.php">tasa de cambio</a>
+                                    <a class="nav-link" href="tblCategoriaGastos.php">Arqueo de caja</a>
+                                    <a class="nav-link" href="tblArqueoCajaDet.php">Arqueo de caja det</a>
+    
+    
+                                   <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                                        <nav class="sb-sidenav-menu-nested nav">
                                             <a class="nav-link" href="login.html">Login</a>
                                             <a class="nav-link" href="register.html">Register</a>
                                             <a class="nav-link" href="password.html">Forgot Password</a>
-                                        </nav> -->
+                                        </nav>
                                     </div>
+                                    
                                     <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-                                        Error
+                                  
+                               
+                                    
+                                </nav>
+                                <a  class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>           
+                                Comunidad
                                         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                                     </a>
                                     <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
                                         <nav class="sb-sidenav-menu-nested nav">
-                                            <a class="nav-link" href="401.html">401 Page</a>
-                                            <a class="nav-link" href="404.html">404 Page</a>
-                                            <a class="nav-link" href="500.html">500 Page</a>
+                                            <a class="nav-link" href="tblcomunidad.php">Comunidades</a>
+                                            <a class="nav-link" href="tblparroquia.php">parroquia</a>
+                                            <a class="nav-link" href="">kermesse</a>
                                         </nav>
-                                    </div>
-                                </nav>
                             </div>
-                            <div class="sb-sidenav-menu-heading">Addons</div>
-                            <a class="nav-link" href="charts.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                                Charts
-                            </a>
-                            <a class="nav-link" href="tables.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                                Tables
-                            </a>
                         </div>
                     </div>
                    
