8ab1b3f6a7d04965dffcde32888ea7e0a41ec520
BenjaminNarvaez-est <benjamin.narvaez13341@est.uca.edu.ni>
2022-11-27 23:23:27 -0600

    Navegación en los editar

--- editarTasaCambio.php
+++ editarTasaCambio.php
@@ -208,7 +208,7 @@ $par= new tasaCambios();
                                             
                                             <div class="form-floating mb-3" style="width: 510px; left: 15px;">                                
                                             <div class="mt-4 mb-0">
-                                                <div class="d-grid"><a class="btn btn-primary btn-block" href="tblparroquia.php">Editar Cambio</a></div>
+                                                <div class="d-grid"><a class="btn btn-primary btn-block" href="tbltasaCambio.php">Editar Cambio</a></div>
                                             </div>
                                         </form>
                                     </div>
